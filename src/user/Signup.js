04603588a21ae73bbe9ddff5d25e8b376ca54338
Syed Muhammad Zaeem Hasan Kazmi <zaeemkazmi@Syeds-MacBook-Pro.local>
2019-06-09 10:13:25 +0200

    Registration from frontend now working correctly with the backend

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,145 +1,169 @@
 import React, { Component } from 'react';
+const axios = require('axios')
 
 class Signup extends Component {
     constructor() {
         super()
         this.state = {
-            "name": "",
-            "email": "",
-            "password": "",
-            "countryOfOrigin": "",
-            "accommName": "",
-            "accommStreet": "",
-            "accommZipcode": "",
-            "accommCity": "",
-            "accommCountry": "",
-            "error": ""
+            name: "",
+            email: "",
+            password: "",
+            countryOfOrigin: "",
+            accommName: "",
+            accommStreet: "",
+            accommZipcode: "",
+            accommCity: "Munich",
+            accommCountry: "Germany",
+            error: "",
+            open: false
         }
     }
 
+
+
     handleChange = name => event => {
-        this.setState({ [name]: event.target.value })
+        this.setState({ error: "" });
+        this.setState({ [name]: event.target.value });
     }
 
     clickSubmit = event => {
         event.preventDefault();
 
-        const {name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode} = this.state;
+        const { name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry } = this.state;
 
         const user = {
-            name : name,
-            email : email,
-            password : password,
-            countryOfOrigin : countryOfOrigin,
-            accommodation : {
-                name : accommName,
-                street : accommStreet,
-                zipcode : accommZipcode,
-                city : "Munich",
-                country : "Germany"
+            name: name,
+            email: email,
+            password: password,
+            countryOfOrigin: countryOfOrigin,
+            accommodation: {
+                name: accommName,
+                street: accommStreet,
+                zipcode: accommZipcode,
+                city: accommCity,
+                country: accommCountry
             }
         };
         console.log(user);
-
-        fetch("http://localhost:8080/users", {
-            method: "POST",
-            header: {
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify(user)
-        }).then(response => {
-            return response.json();
-        }).catch(err => console.log(err));
+        this.signup(user);
     }
 
+    signup = user => {
+        axios
+            .post('http://localhost:8080/users', user)
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    countryOfOrigin: "",
+                    accommName: "",
+                    accommStreet: "",
+                    accommZipcode: "",
+                    accommCity: "Munich",
+                    accommCountry: "Germany",
+                    error: "",
+                    open: true
+                });
+            })
+            .catch(err => {
+                console.error(err.message);
+                this.setState({ error: "Registration failed!" });
+            });
+    }
+
+    signupForm = (name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Display Name</label>
+                <input
+                    onChange={this.handleChange("name")}
+                    type="text"
+                    className="form-control"
+                    value={name}>
+                </input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={this.handleChange("email")}
+                    type="email"
+                    className="form-control"
+                    value={email}>
+                </input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={this.handleChange("password")}
+                    type="password"
+                    className="form-control"
+                    value={password}>
+                </input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Country Of Origin</label>
+                <input onChange={this.handleChange("countryOfOrigin")}
+                    type="text"
+                    className="form-control"
+                    value={countryOfOrigin}>
+                </input>
+            </div>
+
+            <h4 className="mt-5 mb-5">Accommodation Details</h4>
+            <div className="form-group">
+                <label className="text-muted">Building Number</label>
+                <input onChange={this.handleChange("accommName")}
+                    type="text"
+                    className="form-control"
+                    value={accommName}>
+                </input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Street</label>
+                <input onChange={this.handleChange("accommStreet")}
+                    type="text"
+                    className="form-control"
+                    value={accommStreet}>
+                </input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Zip Code</label>
+                <input onChange={this.handleChange("accommZipcode")}
+                    type="text"
+                    className="form-control"
+                    value={accommZipcode}>
+                </input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">City</label>
+                <label className="form-control">{accommCity}</label>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Country</label>
+                <label className="form-control">{accommCountry}</label>
+            </div>
+            <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
+                Register
+                    </button>
+        </form>
+    );
+
     render() {
-        const {name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry} = this.state;
- 
+        const { name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry, error, open } = this.state;
+
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Signup</h2>
 
+                <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
+                    {error}
+                </div>
 
-                <form>
-                    <div className="form-group">
-                        <label className="text-muted">Display Name</label>
-                        <input
-                            onChange={this.handleChange("name")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.name}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Email</label>
-                        <input onChange={this.handleChange("email")}
-                            type="email"
-                            className="form-control"
-                            value={this.state.email}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Password</label>
-                        <input onChange={this.handleChange("password")}
-                            type="password"
-                            className="form-control"
-                            value={this.state.password}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Country Of Origin</label>
-                        <input onChange={this.handleChange("countryOfOrigin")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.countryOfOrigin}>
-                        </input>
-                    </div>
+                <div className="alert alert-success" style={{ display: open ? "" : "none" }}>
+                    New account is successfully created!
+                </div>
 
-                    <h4 className="mt-5 mb-5">Accommodation Details</h4>
-                    <div className="form-group">
-                        <label className="text-muted">Building Number</label>
-                        <input onChange={this.handleChange("accommName")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.accommName}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Street</label>
-                        <input onChange={this.handleChange("accommStreet")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.accommStreet}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Zip Code</label>
-                        <input onChange={this.handleChange("accommZipcode")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.accommZipcode}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">City</label>
-                        <input onChange={this.handleChange("accommCity")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.accommCity}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Country</label>
-                        <input onChange={this.handleChange("accommCountry")}
-                            type="text"
-                            className="form-control"
-                            value={this.state.accommCountry}>
-                        </input>
-                    </div>
-                    <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
-                        Register
-                    </button>
-                </form>
+                {this.signupForm(name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry)}
             </div>
         );
     }
