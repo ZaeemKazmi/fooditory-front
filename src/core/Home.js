cc62333ff21544080e7ae4709cae4c202de9b327
muratguner <muratguner5@gmail.com>
2019-07-07 01:03:56 +0200

    Home page was added (js and css) Login css was added

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+import "./Home.css";
 
 const Home = () => (
-  <div className="jumbotron">
-      <h2>Home</h2>
-      <p className="lead">Welcome to React Frontend</p>
+  <div className="row App__Aside">
+    <div className="col-md-6 leftSide">
+      {" "}
+      <div>
+        <button className="btn btn-lg text-white fbutton"> Offer food</button>{" "}
+      </div>
+      <div>
+        <ul className="rightList">
+          <li>Share fast and simple</li>
+          <li>Avoid wasting food</li>
+          <li>Compensate for groceries</li>
+        </ul>
+      </div>
+    </div>
+    <div className="col-md-6 rightSide">
+      {" "}
+      <div>
+        <button className="btn btn-lg text-white fbutton"> Buy food</button>{" "}
+      </div>
+      <div>
+        <ul className="leftList">
+          <li>Homemade and affordable foods</li>
+          <li>Try different cuisines</li>
+          <li>Save time and make friends</li>
+        </ul>
+      </div>
+    </div>
   </div>
 );
 
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,175 +1,232 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom'
-import { signup , isAuthenticated } from '../auth';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { signup, isAuthenticated } from "../auth";
 
 class Signup extends Component {
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            email: "",
-            password: "",
-            countryOfOrigin: "",
-            accommName: "",
-            accommStreet: "",
-            accommZipcode: "",
-            accommCity: "Munich",
-            accommCountry: "Germany",
-            error: "",
-            open: false
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      countryOfOrigin: "",
+      accommName: "",
+      accommStreet: "",
+      accommZipcode: "",
+      accommCity: "Munich",
+      accommCountry: "Germany",
+      error: "",
+      open: false
+    };
+  }
 
-    handleChange = name => event => {
-        this.setState({ error: "" });
-        this.setState({ open: false });
-        this.setState({ [name]: event.target.value });
-    }
+  handleChange = name => event => {
+    this.setState({ error: "" });
+    this.setState({ open: false });
+    this.setState({ [name]: event.target.value });
+  };
 
-    clickSubmit = event => {
-        event.preventDefault();
+  clickSubmit = event => {
+    event.preventDefault();
 
-        const { name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry } = this.state;
+    const {
+      name,
+      email,
+      password,
+      countryOfOrigin,
+      accommName,
+      accommStreet,
+      accommZipcode,
+      accommCity,
+      accommCountry
+    } = this.state;
 
-        const user = {
-            name: name,
-            email: email,
-            password: password,
-            countryOfOrigin: countryOfOrigin,
-            accommodation: {
-                name: accommName,
-                street: accommStreet,
-                zipcode: accommZipcode,
-                city: accommCity,
-                country: accommCountry
-            }
-        };
-        console.log(user);
-        signup(user).then(response => {
-            console.log(response)
-            this.setState({
-                name: "",
-                email: "",
-                password: "",
-                countryOfOrigin: "",
-                accommName: "",
-                accommStreet: "",
-                accommZipcode: "",
-                accommCity: "Munich",
-                accommCountry: "Germany",
-                error: "",
-                open: true
-            });
-        }).catch(err => {
-            console.log(err.response);
-            if (!(JSON.stringify(err.response.data.error).startsWith("{"))) {
-                this.setState({ error: err.response.data.error });
-            } else {
-                this.setState({ error: "Multiple incorrect values, please fix for your errors and then try again!" });
-            }
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      countryOfOrigin: countryOfOrigin,
+      accommodation: {
+        name: accommName,
+        street: accommStreet,
+        zipcode: accommZipcode,
+        city: accommCity,
+        country: accommCountry
+      }
+    };
+    console.log(user);
+    signup(user)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          name: "",
+          email: "",
+          password: "",
+          countryOfOrigin: "",
+          accommName: "",
+          accommStreet: "",
+          accommZipcode: "",
+          accommCity: "Munich",
+          accommCountry: "Germany",
+          error: "",
+          open: true
         });
-    }
-
-
-
-    signupForm = (name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry) => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">Display Name</label>
-                <input
-                    onChange={this.handleChange("name")}
-                    type="text"
-                    className="form-control"
-                    value={name}>
-                </input>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input onChange={this.handleChange("email")}
-                    type="email"
-                    className="form-control"
-                    value={email}>
-                </input>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input onChange={this.handleChange("password")}
-                    type="password"
-                    className="form-control"
-                    value={password}>
-                </input>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Country Of Origin</label>
-                <input onChange={this.handleChange("countryOfOrigin")}
-                    type="text"
-                    className="form-control"
-                    value={countryOfOrigin}>
-                </input>
-            </div>
-
-            <h4 className="mt-5 mb-5">Accommodation Details</h4>
-            <div className="form-group">
-                <label className="text-muted">Building Number</label>
-                <input onChange={this.handleChange("accommName")}
-                    type="text"
-                    className="form-control"
-                    value={accommName}>
-                </input>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Street</label>
-                <input onChange={this.handleChange("accommStreet")}
-                    type="text"
-                    className="form-control"
-                    value={accommStreet}>
-                </input>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Zip Code</label>
-                <input onChange={this.handleChange("accommZipcode")}
-                    type="text"
-                    className="form-control"
-                    value={accommZipcode}>
-                </input>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">City</label>
-                <label className="form-control">{accommCity}</label>
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Country</label>
-                <label className="form-control">{accommCountry}</label>
-            </div>
-            <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
-                Register
-            </button>
-        </form>
-    );
-
-    render() {
-        const { name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry, error, open } = this.state;
-
-        if(isAuthenticated()){
-            return <Redirect to="/" />
+      })
+      .catch(err => {
+        console.log(err.response);
+        if (!JSON.stringify(err.response.data.error).startsWith("{")) {
+          this.setState({ error: err.response.data.error });
+        } else {
+          this.setState({
+            error:
+              "Multiple incorrect values, please fix for your errors and then try again!"
+          });
         }
+      });
+  };
 
-        return (
-            <div className="container">
-                <h2 className="mt-5 mb-5">Signup</h2>
+  signupForm = (
+    name,
+    email,
+    password,
+    countryOfOrigin,
+    accommName,
+    accommStreet,
+    accommZipcode,
+    accommCity,
+    accommCountry
+  ) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Display Name</label>
+        <input
+          onChange={this.handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={this.handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={this.handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Country Of Origin</label>
+        <input
+          onChange={this.handleChange("countryOfOrigin")}
+          type="text"
+          className="form-control"
+          value={countryOfOrigin}
+        />
+      </div>
 
-                <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
-                    {error}
-                </div>
+      <h4 className="mt-5 mb-5">Accommodation Details</h4>
+      <div className="form-group">
+        <label className="text-muted">Building Number</label>
+        <input
+          onChange={this.handleChange("accommName")}
+          type="text"
+          className="form-control"
+          value={accommName}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Street</label>
+        <input
+          onChange={this.handleChange("accommStreet")}
+          type="text"
+          className="form-control"
+          value={accommStreet}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Zip Code</label>
+        <input
+          onChange={this.handleChange("accommZipcode")}
+          type="text"
+          className="form-control"
+          value={accommZipcode}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">City</label>
+        <label className="form-control">{accommCity}</label>
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Country</label>
+        <label className="form-control">{accommCountry}</label>
+      </div>
+      <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
+        Register
+      </button>
+    </form>
+  );
 
-                <div className="alert alert-success" style={{ display: open ? "" : "none" }}>
-                    New account is successfully created! Please login.
-                </div>
+  render() {
+    const {
+      name,
+      email,
+      password,
+      countryOfOrigin,
+      accommName,
+      accommStreet,
+      accommZipcode,
+      accommCity,
+      accommCountry,
+      error,
+      open
+    } = this.state;
 
-                {this.signupForm(name, email, password, countryOfOrigin, accommName, accommStreet, accommZipcode, accommCity, accommCountry)}
-            </div>
-        );
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
     }
+
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Signup</h2>
+
+        <div
+          className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}
+        >
+          {error}
+        </div>
+
+        <div
+          className="alert alert-success"
+          style={{ display: open ? "" : "none" }}
+        >
+          New account is successfully created! Please login.
+        </div>
+
+        {this.signupForm(
+          name,
+          email,
+          password,
+          countryOfOrigin,
+          accommName,
+          accommStreet,
+          accommZipcode,
+          accommCity,
+          accommCountry
+        )}
+      </div>
+    );
+  }
 }
 
 export default Signup;
